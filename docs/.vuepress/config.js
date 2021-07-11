0d9ca1bba37aee977c014c7cd729edf37ea1ba7c
Spike <l-yanlei@hotmail.com>
2021-07-11 21:19:37 +0800

    feat: hide tag

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     navbar: [
       { text: "前端学习", link: "/front-end/" },
       // { text: "杂", link: "/journey/" },
-      { text: "标签", link: "/tag/" },
+      // { text: "标签", link: "/tag/" },
     ],
     sidebar: {
       "/front-end/": ["make-custom-svg-map.md", "how-to-show-country-flag.md"],
